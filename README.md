0520a31fd2ecb63596920cd565a8fa86e5dc7945
vlappp <vlappp@dell>
2025-07-18 22:27:48 +0700

    Initial commit

--- README.md
+++ README.md
@@ -273,3 +273,4 @@ cloudflared tunnel --url http://127.0.0.1:8000
 cloudflared tunnel --url http://localhost:3000
 npm run dev
 
+# Backend
--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
 
-    allow_origins=["https://feeling-vietnamese-nr-id.trycloudflare.com","http://localhost:3000"],
+    allow_origins=["https://feeling-vietnamese-nr-id.trycloudflare.com","http://localhost:3000","https://chatbot-ui-final.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
